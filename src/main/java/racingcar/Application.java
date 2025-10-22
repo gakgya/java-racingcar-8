f5af737537b3cd6a38e5921ea0fb5af6bc2f764d
gakgya <mirejiho@naver.com>
2025-10-22 17:33:20 +0900

    feat(Application.java): 자동차의 이름 , 최대 전진횟수 입력받는기능 추가
    - 자동차이름 입력받으면 , 를 기준으로 나누기
    - 나눠진 차이름이 5글자를 넘어갈시 IllegalArgumentException 발생
    - 5글자이하 차이름 , 입력된 전진횟수 저장

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,41 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashMap;
+import java.util.Map;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    // Map<String , Integer> carNameMap = new HashMap<>();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String inputCarList = inputCar();
+        Integer inputMaxMove = inputMaxMove();
+        Map<String , Integer> carNameMap = validate(inputCarList);
+    }
+
+    private static String inputCar(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return Console.readLine();
+    }
+
+    private static Integer inputMaxMove(){
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return Integer.parseInt(Console.readLine());
+    }
+
+    private static Map<String , Integer> validate(String inputCarName){
+        String[] carNameArray = inputCarName.split(",");
+        Map<String , Integer> tempMap = new HashMap<>();
+        for (String carName : carNameArray) {
+            if(carName.length() > 5){
+                 throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다: " + carName);
+            }else{
+                tempMap.put(carName, 0);
+            }
+        }
+        return tempMap;
     }
 }
+
