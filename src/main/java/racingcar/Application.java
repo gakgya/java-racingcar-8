653cd12f157e44ded4072fe101afd83c285553a6
gakgya <mirejiho@naver.com>
2025-10-24 14:59:21 +0900

    style,refactor(Application.java): 스타일가이드 요구사항에 맞추기
    스타일가이드에 맞춰서 코드 형태 수정및 요구사항2번에 맞춰 가능한한 메서드가 한개의 역할만 하도록 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,112 +1,116 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.regex.Pattern;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
-    // Map<String , Integer> carNameMap = new HashMap<>();
     private static final Pattern POSITIVE_NUMBER_PATTERN = Pattern.compile("^[1-9]\\d*$");
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         String inputCarList = inputCar();
         Integer inputMaxMove = inputMaxMove();
-        Map<String , Integer> carNameMap = registrationCar(inputCarList);
-        raceStart(carNameMap,inputMaxMove);
+
+        Map<String, Integer> carMap = registerCars(inputCarList);
+        raceStart(carMap, inputMaxMove);
     }
 
-    private static String inputCar(){
+    private static String inputCar() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
 
-    private static Integer inputMaxMove(){
+    private static Integer inputMaxMove() {
         System.out.println("시도할 횟수는 몇 회인가요?");
-        Integer tempMaxMove = inputPositiveNumber(Console.readLine());
-        return tempMaxMove;
+        return inputPositiveNumber(Console.readLine());
     }
 
-    private static Map<String , Integer> registrationCar(String inputCarName){
-        String[] carNameArray = inputCarName.split(",");
-        Map<String , Integer> tempMap = new HashMap<>();
-        for (String carName : carNameArray) {
-            if(!tempMap.containsKey(carName)){
-                tempMap.put(validate(carName),0);
-            }else{
-                throw new IllegalArgumentException("중복차량이 존재합니다! : " + carName);
-            }
+    private static Map<String, Integer> registerCars(String inputCarNames) {
+        String[] carNames = inputCarNames.split(",");
+        Map<String, Integer> carMap = new HashMap<>();
+
+        for (String carName : carNames) {
+            validateDuplicate(carMap, carName);
+            carMap.put(validateCarName(carName), 0);
         }
-        return tempMap;
+        return carMap;
     }
 
-    private static String validate(String carName){
-        if(carName.length() > 5){
+    private static void validateDuplicate(Map<String, Integer> carMap, String carName) {
+        if (carMap.containsKey(carName)) {
+            throw new IllegalArgumentException("중복차량이 존재합니다! : " + carName);
+        }
+    }
+
+    private static String validateCarName(String carName) {
+        if (carName.length() > 5) {
             throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다 : " + carName);
-        }else{
-            return carName;
+        }
+        return carName;
+    }
+
+    private static void raceStart(Map<String, Integer> carMap, Integer maxMove) {
+        for (int i = 0; i < maxMove; i++) {
+            System.out.println();
+            carMap = moveForward(carMap);
+            printRaceProgress(carMap);
+        }
+        printWinner(carMap);
+    }
+
+    private static Map<String, Integer> moveForward(Map<String, Integer> carMap) {
+        for (String car : carMap.keySet()) {
+            carMap.put(car, randomMove(carMap.get(car)));
+        }
+        return carMap;
+    }
+
+    private static int randomMove(int moveCount) {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            return moveCount + 1;
+        }
+        return moveCount;
+    }
+
+    private static void printRaceProgress(Map<String, Integer> carMap) {
+        for (Map.Entry<String, Integer> entry : carMap.entrySet()) {
+            System.out.println(entry.getKey() + " : " + forwardStat(entry.getValue()));
         }
     }
 
-    private static void raceStart(Map<String , Integer> carList , Integer inputMaxMove){
-        for (int i = 0; i < inputMaxMove; i++) {
-            System.out.println("\n");
-            carList = moveFoward(carList);
+    private static String forwardStat(int moveCount) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < moveCount; i++) {
+            builder.append("-");
         }
-        printWinner(carList);
+        return builder.toString();
     }
 
-    private static Map<String , Integer> moveFoward(Map<String , Integer> carList){
-        for (String car : carList.keySet()) {
-            carList.put(car, randomGo(carList.get(car)));
-            System.out.println(car + " : " + forwardStatPrint(carList.get(car)));
-        }
-        return carList;
+    private static void printWinner(Map<String, Integer> carMap) {
+        System.out.println("최종 우승자 : " + String.join(", ", getWinners(carMap)));
     }
 
-    private static Integer randomGo(Integer moveStat){
-        if(Randoms.pickNumberInRange(0, 9)>=4){
-            return moveStat+1;
-        }else{
-            return moveStat;
-        }
-    }
-
-    private static String forwardStatPrint(Integer moveStat){
-        String temp = "";
-        for(int i = 0; i < moveStat; i++){
-            temp += "-";
-        }
-        return temp;
-    }
-
-    private static void printWinner(Map<String , Integer> carList){ 
-        System.out.println("최종 우승자 : " + String.join(", ",getMaxValueKeys(carList)));
-    }
-
-    private static List<String> getMaxValueKeys(Map<String, Integer> carList) {
-        if (carList.isEmpty()) {
+    private static List<String> getWinners(Map<String, Integer> carMap) {
+        if (carMap.isEmpty()) {
             return Collections.emptyList();
         }
-
-        int maxValue = findMaxValue(carList);
-        return findKeysByValue(carList, maxValue);
+        int maxMove = findMaxValue(carMap);
+        return findKeysByValue(carMap, maxMove);
     }
 
-    private static int findMaxValue(Map<String, Integer> carList) {
-        return Collections.max(carList.values());
+    private static int findMaxValue(Map<String, Integer> carMap) {
+        return Collections.max(carMap.values());
     }
 
-    private static List<String> findKeysByValue(Map<String, Integer> carList, int value) {
+    private static List<String> findKeysByValue(Map<String, Integer> carMap, int value) {
         List<String> keys = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : carList.entrySet()) {
+        for (Map.Entry<String, Integer> entry : carMap.entrySet()) {
             if (entry.getValue().equals(value)) {
                 keys.add(entry.getKey());
             }
@@ -115,18 +119,13 @@ public class Application {
     }
 
     private static int inputPositiveNumber(String input) {
-
-        if (isPositiveNumber(input)) {
-            return Integer.parseInt(input);
-        }else{
+        if (!isPositiveNumber(input)) {
             throw new IllegalArgumentException("최대 횟수는 양수만 가능합니다!");
         }
-        
+        return Integer.parseInt(input);
     }
 
     private static boolean isPositiveNumber(String input) {
         return POSITIVE_NUMBER_PATTERN.matcher(input).matches();
     }
-
 }
-
